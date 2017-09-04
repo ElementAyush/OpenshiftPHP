59259abbcdc6f77592b1b6cc0694bffaea36533e
ElementAyush <ele.ayush@hotmail.com>
2017-09-04 15:08:16 +0530

    Signup form

--- index.php
+++ index.php
@@ -3,3 +3,19 @@
  echo "Hello" ;
 
 ?>
+<html>
+<head>
+<title>
+	Sign up Form 
+</title>
+</head>
+<body>
+<form method="post" action="index.php">
+<input type="text" name = "first" placeholder="Enter your first name"> <br>
+<input type="text" name = "last" placeholder="Enter your last name"><br>
+<input type="text" name = "user" placeholder="Enter your user name"><br>
+<input type="text" name = "pass" placeholder="Enter your password"><br>
+<input type="submit" value="signup">
+</form>
+</body>
+</html>
